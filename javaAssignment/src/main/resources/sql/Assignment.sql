cf2c21be783960ad747e11edbab6bb23f2b4c096
Iqbal2016 <iqbal.cse2016@gmail.com>
2023-06-27 00:17:20 +0600

    add sql query

---
+++ javaAssignment/src/main/resources/sql/Assignment.sql
@@ -0,0 +1,19 @@
+----------------CREATE SCHEMA------------------
+CREATE SCHEMA AssignmentDB;
+
+Select * FROM customer_tbl;
+
+TRUNCATE customer_tbl;
+
+CREATE TABLE customer_tbl (
+    id bigint,
+    customerid varchar(10),
+    short_name varchar(20),
+    full_name varchar(60),
+    address1 varchar(80),
+    address2 varchar(80),
+    address3 varchar(80),
+    city varchar(60),
+    postal_code varchar(10) NOT NULL,
+    PRIMARY KEY (id)
+);
